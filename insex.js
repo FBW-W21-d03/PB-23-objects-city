697f339dfd1b69da9c20385431340b7519dca2bf
aisha2000-hub <aisha.noori2000@gmail.com>
2021-12-06 11:07:49 +0100

    Die Lösung von Aisha

---
+++ insex.js
@@ -0,0 +1,17 @@
+// TEIL-1
+const Frankfurt ={
+                Einwohnerzahl: 764.104,
+                Bundesland : "Hessen",
+                Gründungsjahr :"795",
+                Sehenwürdigkeiten : [ "Die frankfurt Altstadt" ," Römer" , "kaiserdom ST.Baetholomäus" , "Main Tower mit Aussichtsplattform ", "städel Museum"]
+}  
+
+
+// TEIL-2
+console.log(Frankfurt);
+
+
+console.log(Frankfurt.Einwohnerzahl);
+console.log(Frankfurt.Bundesland);
+console.log(Frankfurt.Gründungsjahr);
+console.log(Frankfurt.Sehenwürdigkeiten);
