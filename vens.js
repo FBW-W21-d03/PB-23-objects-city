76792526504d179db1e4eb8fdc1a70c4e8f0da36
Vens <v.kolev@alfatrex.com>
2021-12-06 10:34:38 +0100

    vens

---
+++ vens.js
@@ -0,0 +1,14 @@
+const deusseldorf = {
+    einwohnerzahl: 619294,
+    bundesland: "NRW",
+    greundungsjahr: "14 Aug 1288",
+    
+};
+const sehensweurdigkeiten = [
+    "Düsseldorfer Schauspielhaus",
+    "Gustaf-Gründgens-Platz",
+    "Forum Freies Theater",
+    "KaBARett FLiN"
+];
+
+console.log("Düsseldorf:",deusseldorf, "Sehenswürdigkeiten:", sehensweurdigkeiten);
