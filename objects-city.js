97ea54ce5f6ef3bc22aa64fd7773c610ca6ddf5b
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-12-06 10:43:27 +0100

    Orhan's lösung

---
+++ objects-city.js
@@ -0,0 +1,20 @@
+const berlin = {
+    einwohnerzahl: 3766082,
+    bundesland: "Berlin",
+    gründungsjahr: 1237,
+    sehenswürdigkeitten: [
+        "Reichstag",
+        "Brandenburg Gate",
+        "Berlin Television Tower",
+        "Humboldt Forum",
+        "Gendarmenmarkt"
+    ]
+}
+
+console.log("Aufgabe 1): \n", berlin);
+
+
+console.log("Aufgabe 2.1): \n", berlin.einwohnerzahl);
+console.log("Aufgabe 2.2): \n", berlin.bundesland);
+console.log("Aufgabe 2.3): \n", berlin.gründungsjahr);
+console.log("Aufgabe 2.4): \n", berlin.sehenswürdigkeitten);
