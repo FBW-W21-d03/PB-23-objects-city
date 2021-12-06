c555b73ee66b5337a05d1aac6b3a53aa51d53696
Salimov333 <salimov333@gmail.com>
2021-12-06 10:33:44 +0100

    Salem Lösung

---
+++ index.js
@@ -0,0 +1,14 @@
+const Dresden = {
+    Einwohnerzahl: 561.942,
+    Bundesland: "Sachsen",
+    Gründungsjahr: "31. März 1206",
+    Sehenswürdigkeiten: [
+        "Frauenkirche", "Neumarkt", "Semperoper", "Zwinger", "Kathedrale", "Residenzschloss", "Grünes Gewölbe", "Fürstenzug", "Brühlsche Terrasse", "Goldener Reiter", "Albertinum"
+    ]
+}
+
+console.log(Dresden); // object
+console.log("Einwohnerzahl: ", Dresden.Einwohnerzahl); // 561.942
+console.log("Bundesland: ", Dresden.Bundesland); // "Sachsen"
+console.log("Gründungsjahr: ", Dresden.Gründungsjahr); // "31. März 1206"
+console.log("Sehenswürdigkeiten: ", Dresden.Sehenswürdigkeiten); // ["Frauenkirche", "Neumarkt", "Semperoper", "Zwinger", "Kathedrale", "Residenzschloss", "Grünes Gewölbe", "Fürstenzug", "Brühlsche Terrasse", "Goldener Reiter", "Albertinum"]
