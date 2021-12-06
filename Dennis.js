35ddf485f7899dcec95cbcb4353c6cd1fb4a5348
Dennisbullys <d.rochus@hotmail.de>
2021-12-06 10:35:52 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,24 @@
+// javascript-objects-city
+// Aufgabenstellung - Teil 1
+
+
+const lübeck = {
+  Einwohnerzahl: 217.198,
+  Bundesland: "Schleswig Holstein",
+  Gründungsjahr: 1143,
+  Sehenswürdigkeiten:["Lübecker Altstadt","Holstentor","Marienkirche",
+  "Willy-Brandt-Haus"] 
+};
+
+// Aufgabenstellung - Teil 2
+
+console.log(lübeck)
+console.log(lübeck.Einwohnerzahl)
+console.log(lübeck.Bundesland)
+console.log(lübeck.Gründungsjahr)
+console.log(lübeck.Sehenswürdigkeiten)
+
+
+
+
+
