3ae471da0df65574efeab0a286701c4cf09d9e5e
H94ssein44 <alali.52@yahoo.com>
2021-12-06 11:08:25 +0100

    korrektur

--- indx.js
+++ indx.js
@@ -1,11 +1,11 @@
 const Freiburg = {
-einwohnerzahl: 230241 ,
-bundesland: "Baden-wurttemberg",
-gründungsjahr: 1120,
-sehenswürdigkeiten: "Freiburger Münster, Münstermarkt, Universitätsbibliothek, Augustinermuseum",        
+    einwohnerzahl: 230241,
+    bundesland: "Baden-wurttemberg",
+    gründungsjahr: 1120,
+    sehenswürdigkeiten: ["Freiburger Münster", "Münstermarkt", "Universitätsbibliothek", "Augustinermuseum"]
 };
- console.log (Freiburg)
- console.log ("Einwonerzahl:",Freiburg.einwohnerzahl)
- console.log ("Bundesland:",Freiburg.bundesland)
- console.log ("gründungsjahr",Freiburg.gründungsjahr)
- /* console.log ("sehenswürdigkeiten"Freiburg.sehenswürdigkeiten); */
+console.log(Freiburg)
+console.log("Einwonerzahl:", Freiburg.einwohnerzahl)
+console.log("Bundesland:", Freiburg.bundesland)
+console.log("gründungsjahr", Freiburg.gründungsjahr)
+console.log("sehenswürdigkeiten", Freiburg.sehenswürdigkeiten);
