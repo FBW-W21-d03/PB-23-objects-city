70d168829cd6699684bfaedbccb789c987802857
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-12-06 10:39:17 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,15 @@
+const koeln = {
+    Einwohnerzahl: "1,086 Million",
+    Bundesland: "NRW",
+    Gründungsjahr: "38 v.chr",
+    Sehenswürdigkeiten: ["Kölner Dom","Römisches Museum","Schokoladen Museum", "Flora/Botanischer Garten"]
+    };
+    console.log(koeln);
+
+    
+    console.log(koeln.Einwohnerzahl);
+    console.log(koeln.Bundesland);
+    console.log(koeln.Gründungsjahr);
+    console.log(koeln.Sehenswürdigkeiten);
+
+  
