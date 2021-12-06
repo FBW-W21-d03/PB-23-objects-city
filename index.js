b60a143ddbd00f8be8693541af89691fd71b90f4
Mak513 <osei.makangu@hotmail.com>
2021-12-06 10:26:41 +0100

    osei

---
+++ index.js
@@ -0,0 +1,12 @@
+const stadt = {
+    Einwohnerzahl: "1.086 Millionen",
+    Bundesland: "Nord-Rhein-Westfalen",
+    Gründungsjahr: 2000,
+    Sehenswürdigkeiten: ["Dom","Römisches Museum","Schokoladen Museum"]
+};
+
+console.log(stadt);
+console.log(stadt.Einwohnerzahl);
+console.log(stadt.Gründungsjahr);
+console.log(stadt.Sehenswürdigkeiten);
+console.log(stadt.Bundesland);
