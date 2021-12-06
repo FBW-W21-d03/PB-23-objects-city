5c62118c12ff0b9a2bd7b769f1743559c035177d
slavablack <veaceslav.ciornii@magenta.de>
2021-12-06 10:32:19 +0100

    Objekt 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// Objekt
+const berlin = {
+    Einwohnerzahl: "3,645, Millionen",
+    Bundesland: "Berlin",
+    Gründungsjahr: 1237,
+    Fläche:"891,69 km²",
+    Sehenswürdigkeiten: ["Berliner Mauer", "Ferhnsehnturn", "Tierpark", "Brandenburge Tor"],
+ Besonderheiten: ["Das Brandenburger Tor nachts ", "Im Olympia-Stadion aus dem Jahr 1936", "Das Denkmal für die ermordeten Juden Europas","Die Siegessäule ist ein beliebter Treffpunkt"]
+}
+console.log(berlin)
+console.log(berlin.Einwohnerzahl)
+console.log(berlin.Bundesland)
+console.log(berlin.Gründungsjahr)
+console.log(berlin.Fläche)
+console.log(berlin.Sehenswürdigkeiten)
+console.log(berlin.Besonderheiten)
