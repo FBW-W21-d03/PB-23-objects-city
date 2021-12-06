97a39f89fd487d9a644a8cf63870bdcecef742fd
Serdar68 <Serdarbalkis@outlook.de>
2021-12-06 11:05:19 +0100

    Serdar-Berlin

---
+++ java.js
@@ -0,0 +1,11 @@
+const Berlin = {
+    Einwohnerzahl: "3,645 Millionen",
+    Bundesland: "Brandenburg",
+    Gründungsjahr: "1237 Gründungsjahr",
+    Sehenswürdigkeiten: ["Brandenburger-Tor", "Reichstagsgebäude", "Museumsinsel", "Berliner-Dom", "Die-Siegessäule-ist-ein-beliebter-Treffpunkt"]
+};
+
+console.log(Berlin)
+console.log(Berlin.Einwohnerzahl)
+console.log(Berlin.Bundesland, Berlin.Gründungsjahr)
+console.log("Sehenswürdigkeiten sind: ",Berlin.Sehenswürdigkeiten)
