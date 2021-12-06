55263eff752e7ae1548c01bfd73a2e10672f5fdd
sprotte <metatron1972@live.de>
2021-12-06 10:12:49 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const Asgard = {
+    rasse: "Rottweiler",
+    farbe: "Schwarz",
+    geschlecht: "mänlich",
+    alter: 12,
+    besitzer: "Sprotte",
+    geimpft: true
+};
+
+const Rex = {
+    rasse: "Schäferhund",
+    farbe: "braun",
+    geschlecht: "mänlich",
+    alter: 5,
+    besitzer: "Harald",
+    geimpft: true
+};
+
+const Lota = {
+    rasse: "pudel",
+    farbe: "weiß",
+    geschlecht: "weiblich",
+    alter: 4,
+    besitzer: "Sahra",
+    geimpft: true
+};
+
+console.log(Asgard);
+
+let dogs = [];
+dogs.push(Asgard);
+dogs.push(Rex);
+dogs.push(Lota);
+console.log(dogs);
+
