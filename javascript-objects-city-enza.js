d54c458a0b0eda119547c7cbf9cc847d1e1a8a7b
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-06 10:33:16 +0100

    06.12.21 aufgabe01

---
+++ javascript-objects-city-enza.js
@@ -0,0 +1,20 @@
+//Aufgabenstellung - Teil 1
+const hamburg = {
+    stadt: "Hamburg" ,
+    einwohnerzahl: 1851430 ,
+    bundesland: "Hamburg" ,
+    gruendungsjahr: "810" ,
+    sehenswBesonderh: ["Michel" , "Dom" , "Hafen" , "Jungfernstieg" , "Hafenstrasse" , "Rathausmarkt"]
+};
+
+console.log(hamburg);
+
+//Aufgabenstellung - Teil 2
+let stadt = "Hamburg";
+console.log(stadt);
+let einwohnerzahl = 1851430;
+console.log(einwohnerzahl);
+let bundesland = "Hamburg";
+console.log(bundesland);
+let gruendungsjahr = "810";
+console.log(gruendungsjahr);
