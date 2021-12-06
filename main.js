574ec7bc1f3df491479a1cce6dbfbccde091980c
kawanaza <kawanbabani@gmail.com>
2021-12-06 10:42:24 +0100

    Done

---
+++ main.js
@@ -0,0 +1,19 @@
+
+/* Aufgabenstellung - Teil 1 */
+
+const Solingen = {
+    Einwohnerzahl:159360,
+    Bundesland:"NRW",
+    Gründungsjahr: 1480,
+    Sehenswürdigkeiten und Besonderheiten:["Burg Castle","Müngsten Bridge","Deutsches Klingenmuseum","Museum Plagiarius e.V.","Sengbach"],
+    
+
+};
+console.log(Solingen);
+console.log(Solingen.Solingen.Einwohnerzahl);
+console.log(Solingen.Bundesland);
+console.log(Solingen.Sehenswürdigkeiten und Besonderheiten);
+
+
+/* Aufgabenstellung - Teil 2 */
+
