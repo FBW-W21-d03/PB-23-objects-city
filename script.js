ce55a34293f6688a1c8b409246970b20883f6c25
sham912411 <shamalhakim91@gmail.com>
2021-12-06 11:03:46 +0100

    lösung

---
+++ script.js
@@ -0,0 +1,15 @@
+const Düsseldorf = {
+    einwohnerzahl : "620.523",
+    Bundesland: " Nordrhein-Westfalen",
+    Gründungsjahr: "14. August 1288",
+    EinarraymitsehenswürdigkeitenundBesonderheiten: ["Weltarchitektur im MedienHafen "," Rheinturm" , "Königsallee", "Schloss Benrath " ]
+  
+};
+
+/*
+Objekte verwenden
+*/
+console.log( Düsseldorf)
+console.log (Düsseldorf.einwohnerzahl)
+
+
