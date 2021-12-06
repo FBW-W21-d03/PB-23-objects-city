db7856ba7c2de29e7d19b9502492f420b4c2c6d3
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-06 11:03:47 +0100

    js

---
+++ index.js
@@ -0,0 +1,18 @@
+const essen = {
+    Einwohnerzahl: 583.109,
+    Bundesland: "Nordrhein-Westfalen",
+    Gründungsjahr:   800,
+    Sehenswürdigkeiten:["Baldeneysee","Villa Hügel",
+"Grugapark Essen","Zeche Zollverein","Zeche Carl","Essen Kettwig",]
+};
+
+
+
+
+
+
+console.log("Stadt Essen:",essen);
+console.log(essen.Einwohnerzahl);
+console.log(essen.Bundesland);
+console.log(essen.Gründungsjahr);
+console.log(essen.Sehenswürdigkeiten);
