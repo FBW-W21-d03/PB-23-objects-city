961b22a0a95a1b96bc5f2bb470b3fbdf9c696a0a
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-12-06 11:19:29 +0100

    Lösung von Christian

---
+++ christian.js
@@ -0,0 +1,20 @@
+// Teil 1
+const leipzig = {
+  einwohnerzahl: 605407,
+  bundesland: "Sachsen",
+  gruendungsjahr: 1015,
+  sehenswuerdigkeiten: [
+    "Zoo Leipzig",
+    "Museum der bildenden Künste",
+    "Völkerschlachtdenkmal",
+    "Thomaskirche",
+    "Grassi-Museum",
+  ],
+};
+
+// Teil 2
+console.log("Objekt leipzig:", leipzig);
+console.log("Einwohnerzahl:", leipzig.einwohnerzahl);
+console.log("Bundesland:", leipzig.bundesland);
+console.log("Gründungsjahr:", leipzig.gruendungsjahr);
+console.log("Sehenswürdigkeiten:", leipzig.sehenswuerdigkeiten);
